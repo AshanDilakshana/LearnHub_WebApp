47cbec79dbd5e01920d099e984b44aa4cd4e4853
Ashen365 <168340373+Ashen365@users.noreply.github.com>
2025-05-21 17:51:17 +0530

    reedit backend

--- Backend/learnhub/src/main/java/com/paf/learnhub/repositories/CommentRepository.java
+++ Backend/learnhub/src/main/java/com/paf/learnhub/repositories/CommentRepository.java
@@ -1,11 +1,6 @@
-package com.paf.learnhub.repositories ;
+package com.paf.learnhub.repositories;
 
 import com.paf.learnhub.models.Comment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-
-public interface CommentRepository extends MongoRepository<Comment, String> {
-    List<Comment> findByPostId(String postId);
-    List<Comment> findByUserId(String userId);
-}
